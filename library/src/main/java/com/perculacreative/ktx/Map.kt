7bf8792def30d5268b7fc2c85fac748047d1e8b8
pkeefe <pkeefe@haystax.com>
2018-09-17 09:46:18 -0400

    feat: Add replaceWith map extension method

--- library/src/main/java/com/perculacreative/ktx/Map.kt
+++ library/src/main/java/com/perculacreative/ktx/Map.kt
@@ -5,3 +5,9 @@ fun <K, V> Map<K, V?>.filterNotNullValues(): Map<K, V> {
         nullableValue?.let { key to it }
     }.toMap()
 }
+
+fun <T, S> MutableMap<T, S>.replaceWith(map: Map<T, S>) {
+    if (map === this) return
+    clear()
+    putAll(map)
+}
