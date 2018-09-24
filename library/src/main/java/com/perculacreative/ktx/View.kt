5a1831d8dddf78a4b141bd6b0ceb2f3ad63aa0e3
pkeefe <pkeefe@haystax.com>
2018-09-24 09:09:18 -0400

    fix: FindViewById is nullable

--- library/src/main/java/com/perculacreative/ktx/View.kt
+++ library/src/main/java/com/perculacreative/ktx/View.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 
-fun <T : View> Activity.bind(@IdRes idRes: Int): Lazy<T> {
+fun <T : View> Activity.bind(@IdRes idRes: Int): Lazy<T?> {
     return unsafeLazy { findViewById<T>(idRes) }
 }
 
@@ -13,7 +13,7 @@ fun <T : View> Fragment.bind(@IdRes idRes: Int): Lazy<T?> {
     return unsafeLazy { activity?.findViewById<T>(idRes) }
 }
 
-fun <T : View> View.bind(@IdRes idRes: Int): Lazy<T> {
+fun <T : View> View.bind(@IdRes idRes: Int): Lazy<T?> {
     return unsafeLazy { findViewById<T>(idRes) }
 }
 
