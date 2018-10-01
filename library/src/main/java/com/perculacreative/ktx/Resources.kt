3191aa8d660472448686da5f4f9a086d02b6afb6
pkeefe <pkeefe@haystax.com>
2018-10-01 10:53:13 -0400

    feat: Add Resources.getColorCompat()

---
+++ library/src/main/java/com/perculacreative/ktx/Resources.kt
@@ -0,0 +1,14 @@
+package com.perculacreative.ktx
+
+import android.content.res.Resources
+import android.graphics.Color
+import android.os.Build
+import androidx.annotation.ColorRes
+
+fun Resources.getColorCompat(@ColorRes id: Int, theme: Resources.Theme?): Int {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        getColor(id, theme)
+    } else {
+        getColor(id)
+    }
+}
