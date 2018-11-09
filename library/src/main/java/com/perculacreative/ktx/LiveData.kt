9b3b55723580d811b18f6b72dc194c33f4616d16
pkeefe <pkeefe@haystax.com>
2018-11-09 09:41:13 -0500

    feat: Adjust LiveData map/switchmap extension functions to allow null values in results

--- library/src/main/java/com/perculacreative/ktx/LiveData.kt
+++ library/src/main/java/com/perculacreative/ktx/LiveData.kt
@@ -3,10 +3,9 @@ package com.perculacreative.ktx
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 
-fun <X, Y> LiveData<X>.map(mapFunction: (value: X?) -> Y): LiveData<Y> {
-    return Transformations.map(this, mapFunction)
-}
+fun <X, Y> LiveData<X>.map(mapFunction: (value: X?) -> Y?) =
+        Transformations.map(this, mapFunction)
 
-fun <X, Y> LiveData<X>.switchMap(mapFunction: (value: X?) -> LiveData<Y>): LiveData<Y> {
-    return Transformations.switchMap(this, mapFunction)
-}
+
+fun <X, Y> LiveData<X>.switchMap(mapFunction: (value: X?) -> LiveData<Y>): LiveData<Y> =
+        Transformations.switchMap(this, mapFunction)
