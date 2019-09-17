2a9c8296dd28be4dc05870cb0786c7ac5b5b7566
Peter Keefe <16809252+percula@users.noreply.github.com>
2019-09-17 11:28:38 -0400

    fix: Improve bind functions to take into account the lifecycle

--- library/src/main/java/dev/percula/ktx/View.kt
+++ library/src/main/java/dev/percula/ktx/View.kt
@@ -1,20 +1,68 @@
 package dev.percula.ktx
 
-import android.app.Activity
 import android.view.View
 import androidx.annotation.IdRes
+import androidx.core.app.ComponentActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.*
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
-fun <T : View> Activity.bind(@IdRes idRes: Int): Lazy<T?> {
-    return unsafeLazy { findViewById<T>(idRes) }
+/**
+ * Binds the view lazily with consideration of the activity's lifecycle.
+ */
+fun <T : View> ComponentActivity.bind(@IdRes idRes: Int): ReadOnlyProperty<ComponentActivity, T?> {
+    return LifecycleOwnerBinder(this) {
+        it.findViewById<T>(idRes)
+    }
 }
 
-fun <T : View> Fragment.bind(@IdRes idRes: Int): Lazy<T?> {
-    return unsafeLazy { view?.findViewById<T>(idRes) }
+/**
+ * Binds the view lazily with consideration of the fragment's lifecycle.
+ */
+fun <T : View> Fragment.bind(@IdRes idRes: Int): ReadOnlyProperty<Fragment, T?> {
+    return LifecycleOwnerBinder(this) {
+        it.view?.findViewById<T>(idRes)
+    }
 }
 
+/**
+ * Binds the view lazily. Note, if the view is destroyed and recreated, this may
+ * reference a non-existing view.
+ */
 fun <T : View> View.bind(@IdRes idRes: Int): Lazy<T?> {
     return unsafeLazy { findViewById<T>(idRes) }
 }
 
 private fun <T> unsafeLazy(initializer: () -> T) = lazy(LazyThreadSafetyMode.NONE, initializer)
+
+private class LifecycleOwnerBinder<out T : View, S : LifecycleOwner>(
+        val lifecycleOwner: S,
+        val initializer: (S) -> T?
+) : ReadOnlyProperty<S, T?>, LifecycleObserver {
+    private object EMPTY
+    private var viewValue: Any? = EMPTY
+
+    init {
+        if (lifecycleOwner is Fragment) {
+            lifecycleOwner.viewLifecycleOwnerLiveData.observe(lifecycleOwner, Observer {
+                it.lifecycle.addObserver(this)
+            })
+        } else {
+            lifecycleOwner.lifecycle.addObserver(this)
+        }
+    }
+
+    override fun getValue(thisRef: S, property: KProperty<*>): T? {
+        if (viewValue === EMPTY) {
+            viewValue = initializer(lifecycleOwner)
+        }
+        @Suppress("UNCHECKED_CAST")
+        return viewValue as? T
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onViewDestroyed() {
+        viewValue = EMPTY
+    }
+}
