0777258543c15b9505d122da4d898a12f62a1ee5
Peter Keefe <16809252+percula@users.noreply.github.com>
2019-09-17 10:42:41 -0400

    chore: Deprecate LiveData.map{} and LiveData.switchMap{}

--- library/src/main/java/dev/percula/ktx/LiveData.kt
+++ library/src/main/java/dev/percula/ktx/LiveData.kt
@@ -3,9 +3,16 @@ package dev.percula.ktx
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 
+@Deprecated(
+        "Use androidx.lifecycle extension function instead",
+        ReplaceWith("Transformations.map(this, mapFunction)", "androidx.lifecycle.Transformations")
+)
 fun <X, Y> LiveData<X>.map(mapFunction: (value: X?) -> Y?) =
         Transformations.map(this, mapFunction)
 
-
+@Deprecated(
+        "Use androidx.lifecycle extension function instead",
+        ReplaceWith("Transformations.switchMap(this, mapFunction)", "androidx.lifecycle.Transformations")
+        )
 fun <X, Y> LiveData<X>.switchMap(mapFunction: (value: X?) -> LiveData<Y>): LiveData<Y> =
         Transformations.switchMap(this, mapFunction)
