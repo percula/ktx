1acd4119a7aaf84c5cf628e95ef98783278b5396
Peter Keefe <16809252+percula@users.noreply.github.com>
2019-09-17 14:34:43 -0400

    feat: Match enums with toString()

--- library/src/main/java/dev/percula/ktx/Enum.kt
+++ library/src/main/java/dev/percula/ktx/Enum.kt
@@ -1,12 +1,12 @@
 package dev.percula.ktx
 
 /**
- * Returns an enum entry with specified name.
+ * Returns an enum entry with specified name. If the enum has overriden toString(), it will use that
  */
 inline fun <reified T : Enum<T>> enumSafeValueOf(name: String): T? {
     return try {
         enumValueOf<T>(name)
     } catch (e: Exception) {
-        null
+        enumValues<T>().firstOrNull { it.toString() == name }
     }
 }
