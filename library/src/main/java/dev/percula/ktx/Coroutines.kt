7d7bc7e64279742a24e624599da80f26be79ea9f
Peter Keefe <16809252+percula@users.noreply.github.com>
2019-12-19 11:03:17 -0500

    refactor: Flip order of coroutine methods for better auto-import

--- library/src/main/java/dev/percula/ktx/Coroutines.kt
+++ library/src/main/java/dev/percula/ktx/Coroutines.kt
@@ -4,26 +4,27 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-public suspend inline fun <T, R> T.onMain(crossinline block: (T) -> R): R {
-    return withContext(Dispatchers.Main) { this@onMain.let(block) }
-}
 
 public suspend inline fun <T> onMain(crossinline block: CoroutineScope.() -> T): T {
     return withContext(Dispatchers.Main) { block.invoke(this@withContext) }
 }
 
-public suspend inline fun <T, R> T.onDefault(crossinline block: (T) -> R): R {
-    return withContext(Dispatchers.Default) { this@onDefault.let(block) }
+public suspend inline fun <T, R> T.onMain(crossinline block: (T) -> R): R {
+    return withContext(Dispatchers.Main) { this@onMain.let(block) }
 }
 
 public suspend inline fun <T> onDefault(crossinline block: CoroutineScope.() -> T): T {
     return withContext(Dispatchers.Default) { block.invoke(this@withContext) }
 }
 
-public suspend inline fun <T, R> T.onIO(crossinline block: (T) -> R): R {
-    return withContext(Dispatchers.IO) { this@onIO.let(block) }
+public suspend inline fun <T, R> T.onDefault(crossinline block: (T) -> R): R {
+    return withContext(Dispatchers.Default) { this@onDefault.let(block) }
 }
 
 public suspend inline fun <T> onIO(crossinline block: CoroutineScope.() -> T): T {
     return withContext(Dispatchers.IO) { block.invoke(this@withContext) }
+}
+
+public suspend inline fun <T, R> T.onIO(crossinline block: (T) -> R): R {
+    return withContext(Dispatchers.IO) { this@onIO.let(block) }
 }
