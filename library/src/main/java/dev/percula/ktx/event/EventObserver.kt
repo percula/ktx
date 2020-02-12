a83f59c26615325683f2df99e05dca61ca5b2d60
Peter Keefe <16809252+percula@users.noreply.github.com>
2020-02-12 15:20:28 -0500

    fix: EventObserver was not being called

--- library/src/main/java/dev/percula/ktx/event/EventObserver.kt
+++ library/src/main/java/dev/percula/ktx/event/EventObserver.kt
@@ -10,6 +10,6 @@ import androidx.lifecycle.Observer
  */
 class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
     override fun onChanged(event: Event<T>?) {
-        event?.getContentIfNotHandled()?.let { onEventUnhandledContent }
+        event?.getContentIfNotHandled()?.let { onEventUnhandledContent(it) }
     }
 }
