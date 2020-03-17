8a28acb2b3baf67e2b8f66f38d1b988fa2e9f41c
Peter Keefe <16809252+percula@users.noreply.github.com>
2020-03-17 10:49:14 -0400

    fix: use iterator for Menu

--- library/src/main/java/dev/percula/ktx/Menu.kt
+++ library/src/main/java/dev/percula/ktx/Menu.kt
@@ -2,12 +2,12 @@ package dev.percula.ktx
 
 import android.view.Menu
 import android.view.MenuItem
-import androidx.core.view.forEach
+import androidx.core.view.iterator
 
 /** Performs the given action on each item in this menu. */
 inline fun Menu.filter(action: (item: MenuItem) -> Boolean): List<MenuItem> {
     val filteredItems = mutableListOf<MenuItem>()
-    this.forEach {
+    this.iterator().forEach {
         if (action.invoke(it)) filteredItems.add(it)
     }
     return filteredItems
